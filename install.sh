5d016a7c212a47070a88434ea89847c28f48bde5
bjbfr <benjamin.bouchard.fr@gmail.com>
2023-09-15 12:26:11 +0200

    add messages in install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 REPO="https://github.com/bjbfr/gas-projects-template.git"
-# clone repo and create directories
+echo  "==> clone repo and create directories"
 git clone "$REPO" . &&
 mkdir dist && mkdir src && mkdir src/libs && mkdir src/projects
-# install npm packages
+echo  "==> install npm packages"
 yarn install
-# build and link gas-tools
+echo  "==> build and link gas-tools"
 cd gas-tools && mkdir logs &&
 yarn install &&
 yarn build &&
